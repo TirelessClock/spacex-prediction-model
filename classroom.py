b3041bfb55859721a36a934d70b46429a14d3937
Abhimanyu Dayal <abhimanyudayal1@gmail.com>
2022-03-25 00:37:28 +0530

    final ML model built

--- classroom.py
+++ classroom.py
@@ -9,12 +9,22 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import confusion_matrix
 
 data = pd.read_csv("dataset.csv")
+
 y = data["Class"].to_numpy()
 x = pd.read_csv("dataset2.csv")
 
 x = StandardScaler().fit_transform(x)
-x_train, x_test, y_train, y_test = train_test_split(x,y)
+x_train, x_test, y_train, y_test = train_test_split(x,y, test_size=0.1, random_state=2)
 
-print(y_test.shape)
+parameters ={"C":[0.01,0.1,1],'penalty':['l1','l2'], 'solver':['newton-cg', 'lbfgs', 'liblinear','saga']}
+
+logreg_cv = LogisticRegression()
+
+clf = GridSearchCV(logreg_cv ,param_grid = parameters, scoring='accuracy', cv=10)
+clf.fit(x_train, y_train)
+
+print(clf.best_params_)
+print(clf.best_score_)
